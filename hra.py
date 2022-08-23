d29cb80457a30177fda2473a6598dc36fa235274
Ck991234 <94027042+Ck991234@users.noreply.github.com>
2022-08-23 16:44:38 +0530

    Update hra.py
    Update with Folium Map and add animations

--- hra.py
+++ hra.py
@@ -1,15 +1,30 @@
-import streamlit as st
+
 import pandas as pd
 import numpy as np
+from streamlit_folium import st_folium
+import streamlit as st
+import folium
 import geocoder
+import requests
+from streamlit_lottie import st_lottie
+data = "https://github.com/Ck991234/foxy/blob/f29a9bfea28ca68a776687946d914c3319517f23/zomatogeo.csv?raw=true"
+lottie_url ="https://assets2.lottiefiles.com/packages/lf20_mksle47o.json"
 
-url = "https://github.com/Ck991234/foxy/blob/0a27593660add05b10b01230863c07e591328aee/zomatogeo.csv?raw=true"
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-st.title('Pune Hotel Recommendation Engine')
+lottie_json = load_lottieurl(lottie_url)
+st_lottie(lottie_json)
 
-location = st.text_input('Type Any Pune Location, It Will Show You 10 Closest Hotels Near You')
+st.sidebar.title('Pune Hotel Recommendation Engine')
+
+location = st.sidebar.text_input('Type Any Pune Location, It Will Show You 10 Closest Hotels Near You')
 
 #Geo Location Extractor
+
 def get_geolocation (place):
     g = geocoder.arcgis(str(place) +","+"Pune")
     return g.latlng
@@ -20,17 +35,31 @@ st.write('Your Geo Location Is ',latlog[0],latlog[1])
 #KNN Algorithem 
 @st.cache
 def topt(lattitude,longitude):
-    df = pd.read_csv(url,index_col=0)
+    df = pd.read_csv(data,index_col=0)
     inlattitude = lattitude * np.pi/180
     inlongitude = longitude * np.pi/180
     df["e_distance"] = np.sqrt(((inlattitude - df["rad_latit"] )**2) + ((inlongitude - df["rad_longi"])**2))
     df2 = {'Restaurant_Name': 'My Current Location', 'longitude': longitude, 'lattitude':lattitude, 'e_distance':0  }
     df1 = df.append(df2, ignore_index = True)
     final_data = df1.sort_values(["e_distance"],ascending=True).head(10)
-    final_data. rename(columns = {'longitude':'lon', 'lattitude':'lat'}, inplace = True)
+    final_data. rename(columns = {'longitude':'long', 'lattitude':'lat'}, inplace = True)
     return final_data 
 top_20 = topt(latlog[0],latlog[1])
-st.write(top_20["Restaurant_Name"])
-st.map(data = top_20, zoom=None, use_container_width=True)
+st.sidebar.write(top_20["Restaurant_Name"])
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[latlog[0],latlog[1] ], zoom_start=16)
+folium.Marker(
+    [latlog[0], latlog[1]], 
+    popup="Your Location", 
+    tooltip="Your Location"
+).add_to(m)
+
+for i in range (0,len(top_20)):
+    folium.Marker([top_20.iloc[i]["lat"],top_20.iloc[i]["long"]],popup = top_20.iloc[i]["Restaurant_Name"],tooltip = top_20.iloc[i]["Restaurant_Name"]).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width = 600)
+
 
 
